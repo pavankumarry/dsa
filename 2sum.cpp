5308472e49190e89203e31c64b93d37b4953cb55
pavan <pavanyeddula999@gmail.com>
2024-02-29 15:03:17 +0530

    added all possible indices which equal to the sum

--- 2sum.cpp
+++ 2sum.cpp
@@ -1,18 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-pair<int, int> lopp(vector<int> &arr, int target) {
+vector<pair<int, int>> findPairs(vector<int>& arr, int target) {
     map<int, int> mpp;
+    vector<pair<int, int>> result;
     int n = arr.size();
     for (int i = 0; i < n; i++) {
         int value = arr[i];
         int need = target - value;
         if (mpp.find(need) != mpp.end()) {
-            return {mpp[need], i};
+            result.push_back({mpp[need], i});
         }
         mpp[value] = i;
     }
-    return {-1, -1};
+    return result;
 }
 
 int main() {
@@ -21,12 +22,15 @@ int main() {
     cout << "Enter the target value: ";
     cin >> target;
     
-    pair<int, int> result = lopp(arr, target);
+    vector<pair<int, int>> pairs = findPairs(arr, target);
 
-    if (result.first != -1 && result.second != -1) {
-        cout << "Indices: " << result.first << " " << result.second << endl;
+    if (!pairs.empty()) {
+        cout << "Indices that sum up to target:" << endl;
+        for (const auto& pair : pairs) {
+            cout << pair.first << " " << pair.second << endl;
+        }
     } else {
-        cout << "Target not found." << endl;
+        cout << "No pairs found that sum up to target." << endl;
     }
 
     return 0;
